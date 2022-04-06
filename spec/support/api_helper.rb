d1b96a14854052637eb7cab03d2212f0402b9dbb
truonglocbinh <truonglocbinh@MacBook-Pro.local>
2022-04-06 15:34:06 +0700

    fix helper

--- spec/support/api_helper.rb
+++ spec/support/api_helper.rb
@@ -1,5 +1,7 @@
 module ApiHelper
   def get_token(user)
+    user.password = '123456789'
+    user.save
     post "/users/sign_in", params: { user: { email: user.email, password: '123456789' } }, as: :json
     post "/users/sign_in", params: { user: { email: user.email, password: '123456789', otp_attempt: User.find(user.id).current_otp } }, as: :json
     response.headers['Authorization']
