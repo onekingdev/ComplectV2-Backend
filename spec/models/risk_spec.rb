1e131c9b00f95e683b99daa30207e780bc29258b
truonglocbinh <truonglocbinh@MacBook-Pro.local>
2022-04-08 19:10:42 +0700

    add rspec service

--- spec/models/risk_spec.rb
+++ spec/models/risk_spec.rb
@@ -13,4 +13,6 @@ RSpec.describe Risk, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_length_of(:name).is_at_most(255) }
   end
+
+  it { should accept_nested_attributes_for(:policies) }
 end
