021f41305ccdd81ba36cdc14ab59e1be1bc476d1
bruh <bruh@iMac.local>
2022-04-09 20:02:32 +0700

    Fix jwt that outputs in body

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -25,7 +25,7 @@ class Users::SessionsController < Devise::SessionsController
   private
 
   def respond_with(resource, _opts = {})
-    render json: { auth_token: Warden::JWTAuth::UserEncoder.new.call(resource, :users, nil).first }, status: :ok
+    render json: { auth_token: Warden::JWTAuth::UserEncoder.new.call(resource, :user, nil).first }, status: :ok
   end
 
   def respond_to_on_destroy
