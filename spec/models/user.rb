253d3f6ef5d2ce493bcf2683fb117b549ee644aa
truonglocbinh <truonglocbinh@MacBook-Pro.local>
2022-04-11 15:46:09 +0700

    add rspec model

--- spec/models/user.rb
+++ spec/models/user.rb
@@ -5,5 +5,6 @@ RSpec.describe User, type: :model do
     it { should have_many(:exam_requests) }
     it { should have_many(:share_exams) }
     it { should have_many(:share_exams) }
+    it { should have_many(:work_experiences) }
   end
 end
--- spec/models/work_experience_spec.rb
+++ spec/models/work_experience_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe WorkExperience, type: :model do
+  let(:user) { User.create(email: 'complect@gmail.com', password: '123456789', confirmed_at: Time.current) }
   describe 'associations' do
     it { should belong_to(:user) }
   end
@@ -12,5 +13,39 @@ RSpec.describe WorkExperience, type: :model do
     it { should validate_length_of(:employer).is_at_most(255) }
     it { should validate_presence_of(:starts_on) }
     it { should validate_presence_of(:ends_on) }
+  end
+
+  it 'ends_on is required if does not exist is_present' do
+    work_experience = WorkExperience.create(
+      title: 'Ruby',
+      employer: 'complect', 
+      starts_on: Time.current - 1.year,
+      user: user
+    )
+    expect(work_experience.errors.full_messages).to include("Ends on can't be blank")
+  end
+
+  it 'ends_on empty when is_present is true' do
+    work_experience = WorkExperience.create(
+      title: 'Ruby',
+      employer: 'complect', 
+      starts_on: Time.current - 1.year,
+      is_present: true,
+      user: user
+    )
+
+    expect(work_experience.errors.full_messages).to be_empty
+  end
+
+  it 'end date must greater or equal start date' do
+    work_experience = WorkExperience.create(
+      title: 'Ruby',
+      employer: 'complect', 
+      starts_on: Time.current - 1.year,
+      ends_on: Time.current - 2.year,
+      user: user
+    )
+
+    expect(work_experience.errors.full_messages).to include("Starts on can't be greater than end date.")
   end
 end
