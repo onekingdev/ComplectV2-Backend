c0830d118ab9fcdd5bed26d87b5ddf5f3cdfdddc
bruh <bruh@iMac.local>
2022-04-14 23:20:50 +0700

    Remove duplicate authorize def

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,5 @@
 class ApplicationController < ActionController::API
-  include ::Pundit
   before_action :configure_sanitized_params, if: :devise_controller?
-
-  def authorize_action(policy = nil)
-    authorize request.headers['HTTP_BUSINESS_ID'].to_i, policy_class: policy if policy
-  end
 
   def configure_sanitized_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:kind, { profile_attributes: [:first_name, :last_name] }])
